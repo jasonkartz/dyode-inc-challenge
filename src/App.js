447fb32841a59a0ab2d48a3e1fc1db712d7a5ae1
Jason Kartz <jasonkartz@gmail.com>
2022-08-25 18:35:33 -0500

    mobile design finished

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import productImage from "./images/Product-Image.png";
+
 function App() {
   return (
     <>
@@ -12,24 +14,107 @@ function App() {
         </span>
       </header>
       <section className="hero">
-        <div className="hero-image-mobile"></div>
+        <div className="hero-img-mobile hero-img-1"></div>
         <div className="hero-text-mobile">
           <h2 className="header-1">Shop New Arrivals</h2>
           <p>Our coolest new items are waiting for you!</p>
           <button className="btn-main">SHOP NOW</button>
         </div>
       </section>
-      <section class="categories">
-        <div class="category-womens">
-          <h3 class="header-2">Women's</h3>
+      <section className="categories">
+        <div className="category-womens">
+          <h3 className="header-2">Women's</h3>
         </div>
-        <div class="category-mens">
-          <h3 class="header-2">Men's</h3>
+        <div className="category-mens">
+          <h3 className="header-2">Men's</h3>
         </div>
-        <div class="accessories">
-          <h3 class="header-2">Accessories</h3>
+        <div className="accessories">
+          <h3 className="header-2">Accessories</h3>
         </div>
       </section>
+      <section className="new-arrivals">
+        <h2 className="header-1">Shop New Arrivals</h2>
+        <div className="product-slider">
+          <div className="product">
+            <img
+              className="product-img"
+              src={productImage}
+              alt="Women's T-shirt"
+            />
+            <h4 className="product-title">Product Title</h4>
+            <h5 className="product-subtitle">WOMEN'S T-SHIRT</h5>
+            <div className="product-price">$19.99</div>
+          </div>
+          <div className="product">
+            <img
+              className="product-img"
+              src={productImage}
+              alt="Women's T-shirt"
+            />
+            <h4 className="product-title">Product Title</h4>
+            <h5 className="product-subtitle">WOMEN'S T-SHIRT</h5>
+            <div className="product-price">$19.99</div>
+          </div>
+        </div>
+      </section>
+      <section className="hero">
+        <div className="hero-img-mobile hero-img-2"></div>
+        <div className="hero-text-mobile">
+          <h2 className="header-1">Our Favorite Tees</h2>
+          <p>Everyday Tees you need!</p>
+          <button className="btn-main">SHOP NOW</button>
+        </div>
+      </section>
+      <section className="signup">
+        <h3 className="header-2">
+          <i class="ri-mail-fill"></i> <span>Sign up &amp; Stay Connected</span>
+        </h3>
+        <p>
+          Sign up for the newsletter and get 20% off! Gain access to exclusive
+          offers and be the first to know when new stuff drops!
+        </p>
+        <button className="btn-main">SUBSCRIBE</button>
+      </section>
+      <footer>
+        <div class="customer-service">
+          <h2 class="header-footer">
+            <span>Customer Service</span> <i class="ri-add-line"></i>
+          </h2>
+          <ul>
+            <li>Accessibility</li>
+            <li>Contact Us</li>
+            <li>Return Policy</li>
+            <li>FAQ</li>
+            <li>Gift Certificates</li>
+            <li>Wishlist</li>
+          </ul>
+        </div>
+        <div class="company">
+          <h2 class="header-footer">
+            <span>Company</span> <i class="ri-add-line"></i>
+          </h2>
+          <ul>
+            <li>About Us</li>
+            <li>Careers</li>
+            <li>Press</li>
+            <li>Affiliates</li>
+          </ul>
+        </div>
+        <div class="follow-us">
+          <h2 class="header-footer">Follow us</h2>
+          <ul>
+            <li>
+              <i class="ri-facebook-fill"></i>
+            </li>
+            <li>
+              <i class="ri-instagram-line"></i>
+            </li>
+            <li>
+              <i class="ri-twitter-fill"></i>
+            </li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 }
